743f6645db20d8884a81c72a92e2ba543acea645
Óscar <oscar.pozvil@educa.jcyl.es>
2025-04-07 08:39:12 +0200

    Ejercicio7

--- 07/js/script.js
+++ 07/js/script.js
@@ -1,41 +1,41 @@
 document.getElementById("agregarFinal").addEventListener("click", function(){
     // Crear un nuevo párrafo
-   
+    let parrafo=document.createElement("p");
     //añadir texto al párrafo
-  
+    parrafo.innerText=document.getElementById("texto").value;
     //añadir el párrafo al contenedor
-   
+    contenedor.appendChild(parrafo);
     //agregar el color de texto del nuevo parrafo sea rojo
     parrafo.style.color = "red";
 });
 //agregar párrafo al principio
 document.getElementById("agregarPpio").addEventListener("click", function(){
     // Crear un nuevo párrafo
-   
+    let parrafo=document.createElement("p");
     //añadir texto al párrafo
-   
+    parrafo.innerText=document.getElementById("texto").value;
     //añadir el párrafo al contenedor .prepend(parrafo)
-   
+    contenedor.prepend(parrafo);
     //agregar estilos al párrafo texto azul
-   
+    parrafo.style.color="blue";
 });
 //agregar párrafo en una posición concreta
 document.getElementById("agregarPosicion").addEventListener("click", function(){
     // Crear un nuevo párrafo
-  
+    let 
     //añadir texto al párrafo
   
     //añadir el párrafo al contenedor inserBefore
-  
+    document.getElementById("contenedor").insertBefore(parrafo,document.getElementById("contenedor").children[1]);
     //agregar estilos color del texto verde
-    
+    parrafo.style.color="green";
 });
 //eliminar el primer párrafo
 document.getElementById("eliminar").addEventListener("click", function(){
     //eliminar el primer párrafo
-   
+   -
 });
 document.getElementById("eliminarTodos").addEventListener("click", function(){
     //eliminar todos los párrafos
-   
+    document.getElementById("contenedor").innerHTML="";
 });
